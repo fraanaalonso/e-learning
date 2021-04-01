76ed61df47d6679caf2cdd772647596ce1592802
fraanaalonso <fraloal97@gmail.com>
2021-04-01 11:59:02 -0400

    Reflecting the new custom field in our models

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 # Create your models here.
 
 class Subject(models.Model):
@@ -32,9 +33,14 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name='modules', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+    
+    
+    class Meta:
+        ordering = ['order']
     
     def __str__(self):
-        return self.title
+        return f'{self.order}. {self.title}'
     
 
 class Content(models.Model):
@@ -44,6 +50,10 @@ class Content(models.Model):
     })
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+    
+    class Meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
