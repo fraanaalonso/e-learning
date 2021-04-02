34a7f7ac2a77531a28023f40dd10c791ada50192
fraanaalonso <fraloal97@gmail.com>
2021-04-02 12:16:25 -0400

    Enroll action and views to manage detail and list student courses

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,8 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),    
     path('course/', include('courses.urls')),
-    path('admin/', admin.site.urls),
     path('', CourseSubjectListView.as_view(), name="course_subject_list"),
+    path('students/', include('students.urls')),
+    path('admin/', admin.site.urls),
+
 ]
