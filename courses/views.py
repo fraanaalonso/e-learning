1905ec19062fd645560a830ebb16fdce87aeeed4
fraanaalonso <fraloal97@gmail.com>
2021-04-02 12:15:25 -0400

    detail views for student enrolled courses

--- courses/views.py
+++ courses/views.py
@@ -12,6 +12,7 @@ from .models import Module, Content
 from .models import Subject
 from django.db.models import Count
 from django.views.generic.detail import DetailView
+from students.forms import CourseEnrollForm
 # Create your views here.
 
 class OwnerMixin(object):
@@ -163,4 +164,12 @@ class CourseSubjectListView(TemplateResponseMixin, View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/subject/detail.html'
+            #we include the form in the template context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["enroll_form"] = CourseEnrollForm(initial = {'course': self.object}) 
+        return context
+        
+        
+        
     
